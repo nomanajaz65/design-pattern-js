91edc68bede30fcbd95da7346c720005b809f2c8
nomanajaz65 <nomanajaz65@gmail.com>
2023-02-17 01:28:06 -0800

    completed

---
+++ app.js
@@ -0,0 +1,61 @@
+// var arr = ['saad','nomi','sam','tom']
+// for ( var i=0; i< arr.length; i ++){
+//     document.write(arr[i]," ")
+// }
+
+
+
+//  var arr = ['saad','nomi','sam','tom']
+// var arr2 = ['noman']
+
+// for (i=0; i<arr.length;i++){
+// document.write(arr[i]+" " +arr2+ " <br>")
+// }
+
+// var arr = ['saad','nomi','sam','tom']
+// var arr2 = ['noman','nouman']
+
+// for (var i=0; i<arr.length;i++){
+//     for(var j =0; j<arr2.length; j++){
+//      document.write(arr[i]+" " +arr2[j]+ " <br>")
+//     }
+// }
+
+
+// for (var i=1;i<=100 ; i= i+10){
+//    document.write(i,"<br>")
+// }
+
+            //   design patterns
+             
+
+// for (var i=1;i<=100 ; i= i+10){
+// for(var j=i; j <i+10; j++){
+// document.write(j," ")
+// }
+//    document.write("<br>")
+// }
+
+
+// for (var i=1; i<=5; i++){
+//    for (var j=1 ; j<=i; j++){
+//       document.write("*",  ' ')
+//    }
+//    document.write('<BR>')
+// }
+
+           
+// for (var i=1; i<=5; i++){
+//    for (var j=1 ; j<=i; j++){
+//       document.write("j",  ' ')
+//    }
+//    document.write('<BR>')
+// }
+
+
+// for (var i=1; i<=5; i++){
+//    for (var j=1 ; j<=i; j++){
+//        document.write(i,  ' ')
+//    }
+//    document.write('<BR>')
+// }
